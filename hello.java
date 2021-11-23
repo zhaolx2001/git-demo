8d6948718d0b2438be2ae4131937f2e6150e2725
zhaolx <zhaolixin2001@163.com>
2021-11-23 20:24:18 +0800

    第二次提交

--- hello.java
+++ hello.java
@@ -2,11 +2,7 @@ public void hello{
 	
 	public static void main(String[] args ){
 
-		System.out.println("hello !");
-		System.out.println("hello !");
-		System.out.println("hello !");
-		System.out.println("hello !");
-		System.out.println("hello !");
+		System.out.println("hello ! 111111111111111111111111");
 		System.out.println("hello !");
 		System.out.println("hello !");
 		System.out.println("hello !");
