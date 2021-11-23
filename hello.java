4eb1ff73291dc2022c3d05df196e0a3baab21537
zhaolx <zhaolixin2001@163.com>
2021-11-23 21:02:29 +0800

    hot-fix

--- hello.java
+++ hello.java
@@ -2,8 +2,8 @@ public void hello{
 	
 	public static void main(String[] args ){
 
-		System.out.println("hello ! 111111111111111111111111");
-		System.out.println("hello !");
+		System.out.println("hello ! 111111");
+		System.out.println("hello ! 22222222222222222222:");
 		System.out.println("hello !");
 		System.out.println("hello !");
 		System.out.println("hello !");
