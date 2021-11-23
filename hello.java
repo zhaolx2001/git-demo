fb72632bf89ebd8bcdb2270546f0e0393cfab4fd
zhaolx <zhaolixin2001@163.com>
2021-11-23 21:31:16 +0800

    merge 1

--- hello.java
+++ hello.java
@@ -3,7 +3,7 @@ public void hello{
 	public static void main(String[] args ){
 
 		System.out.println("hello ! 111111");
-		System.out.println("hello ! 22222222222222222222:");
+		System.out.println("hello ! 222222");
 		System.out.println("hello !");
 		System.out.println("hello !");
 		System.out.println("hello !");
